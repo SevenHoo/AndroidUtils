e81de9d04515212027d7f52ab6283f24f314f69f
Seven <mr.hoo7793@gmail.com>
2016-12-20 21:07:54 +0800

    fix RandomGenerator bug

--- app/src/main/java/indi/seven/android/utils/math/RandomGenerator.java
+++ app/src/main/java/indi/seven/android/utils/math/RandomGenerator.java
@@ -51,6 +51,9 @@ public class RandomGenerator {
      * @return
      */
     public int getNaturalRandom(int max,int no){
+        if(max <= 1){
+            return 0;
+        }
         int random = no;
         while(random == no){
             random = getNaturalRandom(max);
